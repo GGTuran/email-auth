0c73ec8cf73c9d45967d6c286d70a7f6bc0501c6
GGTuran <tayyabturan67@gmail.com>
2024-02-21 11:30:35 +0600

    adding components

---
+++ src/Component/Login/Login.jsx
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+    return (
+        <div>
+
+            <p>Need to find a login form </p>
+            
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Component/Register/Register.jsx
@@ -0,0 +1,13 @@
+
+
+const Register = () => {
+    return (
+        <div>
+
+            <p>Shall find something to register</p>
+            
+        </div>
+    );
+};
+
+export default Register;
